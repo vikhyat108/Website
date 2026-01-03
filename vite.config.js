b6261f90eda38b123066d0010370dac786ff1c39
vikhyat108 <112775642+vikhyat108@users.noreply.github.com>
2026-01-04 01:24:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/Website/', // Uncomment this line when deploying to GitHub Pages if the repo name matches
+    base: 'https://vikhyat108.github.io/Website/', // Uncomment this line when deploying to GitHub Pages if the repo name matches
 })
